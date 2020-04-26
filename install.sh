e23416c13e7728c46d62d4eddc8e94f813ed24e6
phwes <Philip.Wester@outlook.com>
2020-04-26 15:52:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ mkdir -p /etc/salt/master.d
 touch /etc/salt/master.d/reactor.sls
 
 # If the reactor has been declared, only insert the reactor connection.
-if grep -q 'reactor:' /etc/salt/master.d/reactor.sls; then
+if grep -q 'reactor:' /etc/salt/master.d/reactor.conf; then
   echo "
 - 'salt/minion/*/start':
     - /srv/reactor/mrSaltStack.sls
-" >> /etc/salt/master.d/reactor.sls
+" >> /etc/salt/master.d/reactor.conf
 else
   echo "
 reactor:
 - 'salt/minion/*/start':
     - /srv/reactor/mrSaltStack.sls
-" >> /etc/salt/master.d/reactor.sls
+" >> /etc/salt/master.d/reactor.conf
 fi
 
 mkdir -p /srv/salt
