bcc90493ff5588e98f5bd8cb5b569b97c7b34bbf
phwes <Philip.Wester@outlook.com>
2020-04-26 15:59:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 mkdir -p /etc/salt
 mkdir -p /etc/salt/master.d
-mkdir -p /srv/reactor
 touch /etc/salt/master.d/reactor.sls
+
+mkdir -p /srv/reactor
+mv /src/mrSaltSlack.sls /srv/reactor/mrSaltSlack.sls
 
 # If the reactor has been declared, only insert the reactor connection.
 if grep -q 'reactor:' /etc/salt/master.d/reactor.conf; then
   echo "
 - 'salt/minion/*/start':
-    - /srv/reactor/mrSaltStack.sls
+    - /srv/reactor/mrSaltSlack.sls
 " >> /etc/salt/master.d/reactor.conf
 else
   echo "
 reactor:
 - 'salt/minion/*/start':
-    - /srv/reactor/mrSaltStack.sls
+    - /srv/reactor/mrSaltSlack.sls
 " >> /etc/salt/master.d/reactor.conf
 fi
-
-
-mv /src/mrSaltStack.sls /srv/salt/mrSaltStack.sls
 
 mkdir -p /usr/local/bin/salt_scripts/
 mv send_to_slack.py /usr/local/bin/salt_scripts/send_to_slack.py
@@ -29,4 +28,4 @@ echo 'test-python:
     - tgt: {{ data['id'] }}
     - args:
       - cmd: python /usr/local/bin/salt_scripts/send_to_slack.py
-    ' >> /srv/salt/mrSaltStack.sls
+    ' >> /srv/salt/mrSaltSlack.sls
